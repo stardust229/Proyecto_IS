08b0fae3b3d7c76d580805f7774ab762984ad9b1
Michel Ávila <michelavila229@gmail.com>
2022-04-27 22:45:53 -0500

    Base del proyecto

---
+++ src/test/java/com/example/OlimpiadasUNAM/OlimpiadasUnamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OlimpiadasUNAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlimpiadasUnamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
