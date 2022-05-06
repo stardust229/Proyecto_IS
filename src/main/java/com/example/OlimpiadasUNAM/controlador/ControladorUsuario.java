6a5bd9d44c4a420f2dc37361a50583751844309a
unknown <arely-nava20@ciencias.unam.mx>
2022-05-06 11:53:56 -0500

    consultas

---
+++ src/main/java/com/example/OlimpiadasUNAM/controlador/ControladorUsuario.java
@@ -0,0 +1,54 @@
+package com.example.OlimpiadasUNAM.Controlador;
+
+import com.example.OlimpiadasUNAM.modelo.ModeloUsuario;
+import com.example.OlimpiadasUNAM.Servicio.ServicioUsuario;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ControladorUsuario {
+
+    private final ServicioUsuario servicioUsuario;
+
+    public ControladorUsuario(ServicioUsuario servicioUsuario){
+        this.servicioUsuario = servicioUsuario;
+    }
+
+    @GetMapping("/iniciar_sesion")
+    public String getLoginPage(Model model){
+        model.addAttribute("iniciarSesionRequest", new ModeloUsuario());
+        return "IniciarSesionIH";
+    }
+
+
+    @GetMapping("/registrar")
+    public String getRegisterPage(Model model){
+        model.addAttribute("registrarRequest", new ModeloUsuario());
+        return "registrar_page";
+    }
+
+    @PostMapping("/iniciar_sesion")
+    public String iniciarSesion(@ModelAttribute ModeloUsuario modeloUsuario, Model model){
+        System.out.println("iniciarSesion request: " + modeloUsuario);
+        ModeloUsuario autenticado = servicioUsuario.autenticar(modeloUsuario.getEmail(), modeloUsuario.getPassword());
+        if(autenticado != null){
+            model.addAttribute("iniciarSesionUsuario", autenticado.getEmail());
+            return "PersonalIH";
+        }else
+            return "ErrorIH";
+    }
+
+    @PostMapping("/registrar")
+    public String registrar(@ModelAttribute ModeloUsuario modeloUsuario){
+        System.out.println("registrar request: " + modeloUsuario);
+        ModeloUsuario usuarioRegistrado = servicioUsuario.registrarUsuario(modeloUsuario.getNombre(),modeloUsuario.getEmail(), modeloUsuario.getPassword());
+        return usuarioRegistrado == null ? "ErrorIH" : "redirect:/iniciar_sesion";
+    }
+
+
+
+}
